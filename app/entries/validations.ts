2afe840f4bc41dc267a3fbb0ac1ba8695b1bb115
Christoph Brosch <christoph.brosch@outlook.de>
2022-03-18 13:25:54 +0100

    inital board implementation

--- app/entries/validations.ts
+++ app/entries/validations.ts
@@ -1,9 +1,11 @@
 import { z } from "zod"
 
 const text = z.string()
+const type = z.optional(z.enum(["TEXT", "WARNING", "INFO", "ERROR"]))
 const boardId = z.string()
 
 export const CreateEntrySchema = z.object({
   text,
+  type,
   boardId,
 })
--- app/pages/boards/[boardId].tsx
+++ app/pages/boards/[boardId].tsx
@@ -1,29 +1,28 @@
-import { Suspense, useEffect, useRef } from "react"
+import React, { Suspense, useEffect, useRef } from "react"
 import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Routes } from "blitz"
 import getBoard from "app/boards/queries/getBoard"
 import { DashboardLayout } from "app/core/layouts/DashboardLayout"
 import getEntries from "app/entries/queries/getEntries"
 import { Plain, Termynal } from "react-termynal"
+import { Typography } from "@mui/material"
 
 export const Board = () => {
   const boardId = useParam("boardId", "string")
   const [board] = useQuery(getBoard, { id: boardId })
-  const [{ entries }, { refetch }] = useQuery(
+  const [{ entries }] = useQuery(
     getEntries,
     { where: { boardId: boardId } },
     { refetchInterval: 5000 }
   )
-
   return (
     <div style={{ position: "relative", height: "100%" }}>
-      {console.log(entries)}
       <Head>
         <title>Board {board.name}</title>
       </Head>
-      <Termynal id="placeholder" name="bash">
-        {entries.map((entry) => (
-          <Plain key={entry.id}>
-            <p>{entry.text}</p>
+      <Termynal>
+        {entries.map((entry, index) => (
+          <Plain lineNumber={index + 1} key={entry.text}>
+            <Typography color={entry.type.toLowerCase() + ".main"}>{entry.text}</Typography>
           </Plain>
         ))}
       </Termynal>
@@ -39,7 +38,7 @@ const ShowBoardPage: BlitzPage = () => {
   )
 }
 
-ShowBoardPage.authenticate = true
+ShowBoardPage.authenticate = { redirectTo: Routes.LoginPage() }
 ShowBoardPage.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>
 
 export default ShowBoardPage
